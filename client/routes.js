7ba0fa12e4992f9dfbce35bf75a1b41579b2df13
phatNguyen <makeapp4you@gmail.com>
2017-06-29 15:46:21 +0700

    login and validate

--- client/routes.js
+++ client/routes.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
+import LoginPage from './components/login/LoginPage';
+
 import App from './components/App';
 import Contact from './components/Contact';
 import SignUpPage from './components/signup/SignUpPage';
@@ -9,5 +11,6 @@ export default (
     <Route path="/" component={App}>
         <IndexRoute component={Contact} />
         <Route path="signup" component={SignUpPage} />
+        <Route path="login" component={LoginPage} />
     </Route>
 )
